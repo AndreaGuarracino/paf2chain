d069fa22a350127a0718f4c990c6619ce865d830
AndreaGuarracino <andreaguarracino@outlook.com>
2021-08-07 11:28:00 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ extern crate clap;
 use clap::{App, Arg}; //, SubCommand};
 
 mod paf;
-use crate::paf::PafFile;
+use crate::paf::{PafFile, paf_query_end, paf_target, paf_target_length, paf_target_begin, paf_target_end, paf_query, paf_query_length, paf_query_is_rev, paf_query_begin};
 
 fn main() {
     let matches = App::new("paf2chain")
@@ -30,74 +30,156 @@ fn main() {
                 .long("input")
                 .help("input PAF file"),
         )
-        .arg(
-            Arg::with_name("OUTPUT")
-                .takes_value(true)
-                .short("o")
-                .long("output")
-                .help("save the CHAIN format output to this file."),
-        )
         .get_matches();
 
     let filename = matches.value_of("INPUT").unwrap();
     let paf = PafFile::new(filename);
 
-    let default_output = format!("{}.chain", filename);
-    let output_chain = matches.value_of("output").unwrap_or(&default_output);
-
-    // // draw our grid
-    // paf.targets.iter().for_each(|target| {
-    //     if target.offset > 0 {
-    //         let start = get_coords(target.offset, 0);
-    //         let end = get_coords(target.offset, paf.query_length as usize);
-    //         for ((i, j), val) in XiaolinWu::<f64, i64>::new(start, end) {
-    //             if i >= 0 && i < (axes.0 as i64) && j >= 0 && j < (axes.1 as i64) {
-    //                 let i: usize = (i as usize) + (((axes.1 - 1) - j as usize) * axes.0);
-    //                 pixels[i] = get_color(val * 0.2);
-    //             }
-    //         }
-    //     }
-    // });
-    // paf.queries.iter().for_each(|query| {
-    //     if query.offset > 0 {
-    //         let start = get_coords(0, query.offset);
-    //         let end = get_coords(paf.target_length as usize, query.offset);
-    //         for ((i, j), val) in XiaolinWu::<f64, i64>::new(start, end) {
-    //             if i >= 0 && i < (axes.0 as i64) && j >= 0 && j < (axes.1 as i64) {
-    //                 let i: usize = (i as usize) + (((axes.1 - 1) - j as usize) * axes.0);
-    //                 pixels[i] = get_color(val * 0.2);
-    //             }
-    //         }
-    //     }
-    // });
-
     // for each match, we draw a line on our raster using Xiaolin Wu's antialiased line algorithm
-    let draw_match = |_c, x: usize, rev: bool, y: usize, len: usize| {
-        // //println!("draw_match {} {} {} {} {}", _c, x, rev, y, len);
-        // let start = get_coords(x, y);
-        // let end = get_coords(x + if rev { 0 - len } else { len }, y + len);
-        // /*
-        // println!(
-        //     "start and end ({} {}) ({} {})",
-        //     start.0, start.1, end.0, end.1
-        // );
-        //  */
-        // for ((j, i), val) in XiaolinWu::<f64, i64>::new(start, end) {
-        //     //println!("checking pixel {} {} {}", i, j, val);
-        //     if i >= 0 && i < (axes.0 as i64) && j >= 0 && j < (axes.1 as i64) {
-        //         //println!("drawing pixel {} {} {}", i, j, val);
-        //         let i: usize = (i as usize) + (((axes.1 - 1) - j as usize) * axes.0);
-        //         pixels[i] = get_color(val);
-        //     }
-        // }
+    let draw_match = |line: &str| {
+        let query_rev = paf_query_is_rev(line);
+        let (x, y) = paf.global_start(line, query_rev);
+        let mut target_pos = x;
+        let mut query_pos = y;
+        // find and walk the cigar string
+        //println!("{}", line);
+        let mut cigars = line
+            .split('\t')
+            .skip_while(|s| !s.starts_with("cg:Z:"))
+            .map(|s| s.strip_prefix("cg:Z:").unwrap())
+            .collect::<Vec<&str>>();
+
+        // Compute the fake CIGAR, in case the real one is missing in the line
+        let query_len = paf_query_end(line) - paf_query_begin(line);
+        let target_len = paf_target_end(line) - paf_target_begin(line);
+        let fake_cigar = format!(
+            "{}M",
+            if query_len < target_len {
+                query_len
+            } else {
+                target_len
+            }
+        );
+        if cigars.is_empty() {
+            cigars.push(fake_cigar.as_str());
+        }
+
+        let score = 255;
+        let t_name = paf_target(line);
+        let t_size = paf_target_length(line);
+        let t_strand = "+";
+        let t_start = paf_target_begin(line);
+        let t_end = paf_target_end(line);
+        let q_name = paf_query(line);
+        let q_size = paf_query_length(line);
+        let q_strand = if paf_query_is_rev(line) { "-"} else {"+"};
+        let q_start = paf_query_begin(line);
+        let q_end = paf_query_end(line);
+        let id = 0;
+        for mut cigar in cigars {
+            //println!("{}", cigar);
+
+            let mut first: usize = 0;
+
+            // Trim INDELs at the beginning and at the end of the CIGAR string
+            let mut first_not_indel_found = false;
+            let mut trim_from: usize = 0;
+            let mut query_from_delta: usize = 0;
+            let mut target_from_delta: usize = 0;
+            let mut trim_to: usize = 0;
+            let mut query_to_delta: usize = 0;
+            let mut target_to_delta: usize = 0;
+            for (i, b) in cigar.bytes().enumerate() {
+                let c = b as char;
+                //println!("{} {}", i, b as char);
+                match c {
+                    'M' | '=' | 'X' => {
+                        first = i + 1;
+
+                        trim_to = i + 1;
+                        query_to_delta = 0;
+                        target_to_delta = 0;
+
+                        first_not_indel_found = true;
+                    }
+                    'D' => {
+                        let n = cigar[first..i].parse::<usize>().unwrap();
+                        first = i + 1;
+
+                        if !first_not_indel_found {
+                            trim_from = i + 1;
+                            target_from_delta += n;
+                        }
+
+                        target_to_delta += n;
+                    }
+                    'I' => {
+                        let n = cigar[first..i].parse::<usize>().unwrap();
+                        first = i + 1;
+
+                        if !first_not_indel_found {
+                            trim_from = i + 1;
+                            query_from_delta += n;
+                        }
+
+                        query_to_delta += n;
+                    }
+                    _ => {}
+                }
+            }
+
+            cigar = &cigar[trim_from..trim_to];
+
+            // Header line
+            println!("chain\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}", score, t_name, t_size, t_strand, t_start + target_from_delta, t_end - target_to_delta, q_name, q_size, q_strand, q_start + query_from_delta, q_end - query_to_delta, id);
+
+            let mut ungapped_alignment_len: usize = 0;
+            let mut query_delta: usize = 0;
+            let mut target_delta: usize = 0;
+
+            first = 0;
+            for (i, b) in cigar.bytes().enumerate() {
+                let c = b as char;
+                //println!("{} {}", i, b as char);
+                match c {
+                    'M' | '=' | 'X' => {
+                        let n = cigar[first..i].parse::<usize>().unwrap() as usize;
+                        query_pos += if query_rev { 0 - n } else { n };
+                        target_pos += n;
+                        first = i + 1;
+
+                        if ungapped_alignment_len > 0 && (query_delta > 0 || target_delta > 0) {
+                            println!("{}\t{}\t{}", ungapped_alignment_len, target_delta, query_delta);
+
+                            ungapped_alignment_len = 0;
+                        }
+
+                        target_delta = 0;
+                        query_delta = 0;
+
+                        ungapped_alignment_len += n;
+                    }
+                    'D' => {
+                        let n = cigar[first..i].parse::<usize>().unwrap();
+                        target_pos += n;
+                        first = i + 1;
+
+                        target_delta += n;
+                    }
+                    'I' => {
+                        let n = cigar[first..i].parse::<usize>().unwrap();
+                        query_pos += if query_rev { 0 - n } else { n };
+                        first = i + 1;
+
+                        query_delta += n;
+                    }
+                    _ => {}
+                }
+            }
+
+            println!("{}", ungapped_alignment_len);
+        }
+        println!();
     };
-    paf.for_each_match_in_file(draw_match);
-
-    let path = &Path::new(output_chain);
-
-    // encode_file takes the path to the image, a u8 array,
-    // the width, the height, the color mode, and the bit depth
-    // if let Err(e) = lodepng::encode_file(path, &raw, axes.0, axes.1, lodepng::ColorType::RGB, 8) {
-    //     panic!("failed to write png: {:?}", e);
-    // }
+    paf.for_each_line_in_file(draw_match);
 }
--- src/paf.rs
+++ src/paf.rs
@@ -43,43 +43,43 @@ pub struct PafFile {
     query_length: f64,
 }
 
-fn paf_query(line: &str) -> String {
+pub fn paf_query(line: &str) -> String {
     line.split('\t').next().unwrap().into()
 }
 
-fn paf_query_length(line: &str) -> usize {
+pub fn paf_query_length(line: &str) -> usize {
     line.split('\t').nth(1).unwrap().parse::<usize>().unwrap()
 }
 
-fn paf_query_begin(line: &str) -> usize {
+pub fn paf_query_begin(line: &str) -> usize {
     line.split('\t').nth(2).unwrap().parse::<usize>().unwrap()
 }
 
-fn paf_query_end(line: &str) -> usize {
+pub fn paf_query_end(line: &str) -> usize {
     line.split('\t').nth(3).unwrap().parse::<usize>().unwrap()
 }
 
-fn paf_query_is_rev(line: &str) -> bool {
+pub fn paf_query_is_rev(line: &str) -> bool {
     line.split('\t').nth(4).unwrap() == "-"
 }
 
-fn paf_target(line: &str) -> String {
+pub fn paf_target(line: &str) -> String {
     line.split('\t').nth(5).unwrap().into()
 }
 
-fn paf_target_length(line: &str) -> usize {
+pub fn paf_target_length(line: &str) -> usize {
     line.split('\t').nth(6).unwrap().parse::<usize>().unwrap()
 }
 
-fn paf_target_begin(line: &str) -> usize {
+pub fn paf_target_begin(line: &str) -> usize {
     line.split('\t').nth(7).unwrap().parse::<usize>().unwrap()
 }
 
-fn paf_target_end(line: &str) -> usize {
+pub fn paf_target_end(line: &str) -> usize {
     line.split('\t').nth(8).unwrap().parse::<usize>().unwrap()
 }
 
-fn for_each_line_in_file(paf_filename: &str, mut callback: impl FnMut(&str)) {
+fn _for_each_line_in_file(paf_filename: &str, mut callback: impl FnMut(&str)) {
     let file = File::open(paf_filename).unwrap();
     let reader = BufReader::new(file);
     for line in reader.lines() {
@@ -91,7 +91,7 @@ impl PafFile {
     pub fn new(filename: &str) -> Self {
         let mut query_names: Vec<String> = Vec::new();
         let mut target_names: Vec<String> = Vec::new();
-        for_each_line_in_file(filename, |l: &str| {
+        _for_each_line_in_file(filename, |l: &str| {
             query_names.push(paf_query(l));
             target_names.push(paf_target(l));
         });
@@ -104,7 +104,7 @@ impl PafFile {
         let mut seen_targets = vec![false; target_names.len()];
         let mut queries: Vec<AlignedSeq> = vec![AlignedSeq::new(); query_names.len()];
         let mut targets: Vec<AlignedSeq> = vec![AlignedSeq::new(); target_names.len()];
-        for_each_line_in_file(filename, |l: &str| {
+        _for_each_line_in_file(filename, |l: &str| {
             let query_name: String = paf_query(l);
             let query_id = query_mphf.hash(&query_name) as usize;
             if !seen_queries[query_id] {
@@ -192,7 +192,7 @@ impl PafFile {
     fn target_length(self: &PafFile, idx: usize) -> usize {
         self.targets[idx].length
     }
-    fn global_start(self: &PafFile, line: &str, query_rev: bool) -> (usize, usize) {
+    pub(crate) fn global_start(self: &PafFile, line: &str, query_rev: bool) -> (usize, usize) {
         let query_id = self.query_mphf.hash(&paf_query(line)) as usize;
         let target_id = self.target_mphf.hash(&paf_target(line)) as usize;
         (
@@ -205,226 +205,12 @@ impl PafFile {
         )
     }
 
-    fn for_each_match<F>(self: &PafFile, line: &str, mut func: F)
+    pub(crate) fn for_each_line_in_file<F>(self: &PafFile, mut func: F)
         where
-            F: FnMut(char, usize, bool, usize, usize),
+            F: FnMut(&str),
     {
-        let query_rev = paf_query_is_rev(line);
-        let (x, y) = self.global_start(line, query_rev);
-        let mut target_pos = x;
-        let mut query_pos = y;
-        // find and walk the cigar string
-        //println!("{}", line);
-        let mut cigars = line
-            .split('\t')
-            .skip_while(|s| !s.starts_with("cg:Z:"))
-            .map(|s| s.strip_prefix("cg:Z:").unwrap())
-            .collect::<Vec<&str>>();
-
-        // Compute the fake CIGAR, in case the real one is missing in the line
-        let query_len = paf_query_end(line) - paf_query_begin(line);
-        let target_len = paf_target_end(line) - paf_target_begin(line);
-        let fake_cigar = format!(
-            "{}M",
-            if query_len < target_len {
-                query_len
-            } else {
-                target_len
-            }
-        );
-        if cigars.is_empty() {
-            cigars.push(fake_cigar.as_str());
-        }
-
-        let score = 255;
-        let t_name = paf_target(line);
-        let t_size = paf_target_length(line);
-        let t_strand = "+";
-        let t_start = paf_target_begin(line);
-        let t_end = paf_target_end(line);
-        let q_name = paf_query(line);
-        let q_size = paf_query_length(line);
-        let q_strand = if paf_query_is_rev(line) { "-"} else {"+"};
-        let q_start = paf_query_begin(line);
-        let q_end = paf_query_end(line);
-        let id = 0;
-        for mut cigar in cigars {
-            //println!("{}", cigar);
-
-            let mut first: usize = 0;
-
-            // Trim INDELs at the beginning and at the end of the CIGAR string
-            let mut first_not_indel_found = false;
-            let mut trim_from: usize = 0;
-            let mut query_from_delta: usize = 0;
-            let mut target_from_delta: usize = 0;
-            let mut trim_to: usize = 0;
-            let mut query_to_delta: usize = 0;
-            let mut target_to_delta: usize = 0;
-            for (i, b) in cigar.bytes().enumerate() {
-                let c = b as char;
-                //println!("{} {}", i, b as char);
-                match c {
-                    'M' | '=' | 'X' => {
-                        first = i + 1;
-
-                        trim_to = i + 1;
-                        query_to_delta = 0;
-                        target_to_delta = 0;
-
-                        first_not_indel_found = true;
-                    }
-                    'D' => {
-                        let n = cigar[first..i].parse::<usize>().unwrap();
-                        first = i + 1;
-
-                        if !first_not_indel_found {
-                            trim_from = i + 1;
-                            target_from_delta += n;
-                        }
-
-                        target_to_delta += n;
-                    }
-                    'I' => {
-                        let n = cigar[first..i].parse::<usize>().unwrap();
-                        first = i + 1;
-
-                        if !first_not_indel_found {
-                            trim_from = i + 1;
-                            query_from_delta += n;
-                        }
-
-                        query_to_delta += n;
-                    }
-                    _ => {}
-                }
-            }
-
-            cigar = &cigar[trim_from..trim_to];
-
-            // Header line
-            println!("chain\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}", score, t_name, t_size, t_strand, t_start + target_from_delta, t_end - target_to_delta, q_name, q_size, q_strand, q_start + query_from_delta, q_end - query_to_delta, id);
-
-            let mut ungapped_alignment_len: usize = 0;
-            let mut query_delta: usize = 0;
-            let mut target_delta: usize = 0;
-
-            first = 0;
-            for (i, b) in cigar.bytes().enumerate() {
-                let c = b as char;
-                //println!("{} {}", i, b as char);
-                match c {
-                    'M' | '=' | 'X' => {
-                        let n = cigar[first..i].parse::<usize>().unwrap() as usize;
-                        func(c, query_pos, query_rev, target_pos, n);
-                        query_pos += if query_rev { 0 - n } else { n };
-                        target_pos += n;
-                        first = i + 1;
-
-                        if ungapped_alignment_len > 0 && (query_delta > 0 || target_delta > 0) {
-                            println!("{}\t{}\t{}", ungapped_alignment_len, target_delta, query_delta);
-
-                            ungapped_alignment_len = 0;
-                        }
-
-                        target_delta = 0;
-                        query_delta = 0;
-
-                        ungapped_alignment_len += n;
-                    }
-                    'D' => {
-                        let n = cigar[first..i].parse::<usize>().unwrap();
-                        target_pos += n;
-                        first = i + 1;
-
-                        target_delta += n;
-                    }
-                    'I' => {
-                        let n = cigar[first..i].parse::<usize>().unwrap();
-                        query_pos += if query_rev { 0 - n } else { n };
-                        first = i + 1;
-
-                        query_delta += n;
-                    }
-                    _ => {}
-                }
-            }
-
-            println!("{}", ungapped_alignment_len);
-        }
-        println!();
-    }
-
-    pub(crate) fn for_each_match_in_file<F>(self: &PafFile, mut func: F)
-        where
-            F: FnMut(char, usize, bool, usize, usize),
-    {
-        for_each_line_in_file(&self.filename, |line: &str| {
-            /*let (x, y) = self.global_start(line, paf_query_is_rev(line));
-            println!(
-                "{} {} {} {} {} {}",
-                paf_query(line),
-                paf_query_begin(line),
-                paf_target(line),
-                paf_target_begin(line),
-                x,
-                y
-            );*/
-
-            self.for_each_match(line, |c, x, r, y, d| func(c, x, r, y, d));
-            //println!();
+        _for_each_line_in_file(&self.filename, |line: &str| {
+            func(line);
         });
     }
-    fn get_axes(self: &PafFile, major_axis: usize) -> (usize, usize) {
-        //let max_length = cmp::max(self.query_length, self.target_length) as f64;
-        //println!("query = {} target = {}", self.query_length, self.target_length);
-        if self.target_length > self.query_length {
-            let ratio = self.query_length as f64 / self.target_length as f64;
-            (major_axis, (major_axis as f64 * ratio) as usize)
-        } else {
-            let ratio = self.target_length as f64 / self.query_length as f64;
-            ((major_axis as f64 * ratio) as usize, major_axis)
-        }
-    }
-    fn get_axes_zoom(
-        self: &PafFile,
-        major_axis: usize,
-        zoom: ((usize, usize), (usize, usize)),
-    ) -> (usize, usize) {
-        let t_length = zoom.0.1 - zoom.0.0;
-        let q_length = zoom.1.1 - zoom.1.0;
-        if t_length > q_length {
-            let ratio = q_length as f64 / t_length as f64;
-            (major_axis, (major_axis as f64 * ratio) as usize)
-        } else {
-            let ratio = t_length as f64 / q_length as f64;
-            ((major_axis as f64 * ratio) as usize, major_axis)
-        }
-    }
-    fn project_xy(self: &PafFile, x: usize, y: usize, axes: (usize, usize)) -> (f64, f64) {
-        //println!("axes {} {}", axes.0, axes.1);
-        (
-            axes.0 as f64 * (x as f64 / self.target_length as f64),
-            axes.1 as f64 * (y as f64 / self.query_length as f64),
-        )
-    }
-    fn project_xy_zoom(
-        self: &PafFile,
-        x: usize,
-        y: usize,
-        axes: (usize, usize),
-        zoom: ((usize, usize), (usize, usize)),
-    ) -> (f64, f64) {
-        //println!("axes {} {}", axes.0, axes.1);
-        (
-            axes.0 as f64 * (((x as f64) - (zoom.0.0 as f64)) / ((zoom.0.1 - zoom.0.0) as f64)),
-            axes.1 as f64 * (((y as f64) - (zoom.1.0 as f64)) / ((zoom.1.1 - zoom.1.0) as f64)),
-        )
-    }
-    /*
-        fn get_pixel(self: &PafFile, x: i64, y: i64, axes: (usize, usize)) -> usize {
-            let (q, t) = axes;
-            x * (self.query_length / q as f64).round() as usize * q + y * (self.target_length / t as f64).round() as usize
-        }
-    */
 }
